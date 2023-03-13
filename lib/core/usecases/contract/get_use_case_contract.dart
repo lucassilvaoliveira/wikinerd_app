0119ad660953b0adf0ef33758041d060ab46796a
dev-lucas <silvaluxas5@gmail.com>
2023-03-13 12:34:12 -0300

    changing getUseCaseContract to abstract class

--- lib/core/usecases/contract/get_use_case_contract.dart
+++ lib/core/usecases/contract/get_use_case_contract.dart
@@ -1,7 +1,7 @@
 import 'package:multiple_result/multiple_result.dart';
 import 'package:wikinerd/core/usecases/contract/use_case_contract.dart';
 
-class GetUseCaseContract<T, E> extends UseCaseContract<T, E> {
+abstract class GetUseCaseContract<T, E> extends UseCaseContract<T, E> {
   GetUseCaseContract({required super.repository});
 
   @override
